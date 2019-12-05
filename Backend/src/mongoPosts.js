94cadcaa5755a87791070412214900985b477e3a
Connor Grimm <cagrimm13@gmail.com>
2019-12-04 17:42:00 -0700

    Update mongoPosts.js

--- Backend/src/mongoPosts.js
+++ Backend/src/mongoPosts.js
@@ -1,4 +1,5 @@
 import { getDBConnection } from './mongoConnection'
+import { ObjectID } from 'mongodb'
 
 let addLowerItemName =  (post) => {
     post.itemNameLower = post.item.itemName.toLowerCase()
@@ -8,7 +9,6 @@ module.exports.getPosts = async (keyWord) => {
     let connection = await getDBConnection()
     let db = connection["db"]
     let reg = new RegExp("%" + keyWord ? keyWord.toLowerCase() : "" + "%")
-    console.log(reg)
     let resp = await db.collection("Posts").find({
         itemNameLower: reg
     }, {
@@ -38,3 +38,27 @@ module.exports.createPost = async (postInfo) => {
     connection["client"].close()
     return resp.ops.length == 1 ? resp.ops[0] : resp.ops
 }
+
+module.exports.getUserPosts = async (userID) => {
+    let connection = await getDBConnection()
+    let db = connection["db"]
+    let resp = await db.collection("Posts").find({
+        "user.userID": new ObjectID(userID)
+    }, {
+        sort: [["_id", "desc"]]
+    }).toArray()
+    connection["client"].close()
+    return resp
+}
+
+module.exports.deletePost = async (postID) => {
+    let connection = await getDBConnection()
+    let db = connection["db"]
+    let resp = await db.collection("Posts").deleteOne({
+        "_id": new ObjectID(postID)
+    })
+    connection["client"].close()
+    return resp
+}
+
+module.exports.deletePost("5de71bc4b06d0247b254d7a6")
