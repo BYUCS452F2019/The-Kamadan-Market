7ca56e5a45a254ae481f57245afec02ead98fb01
Danny Spencer <therealdannyspencer@gmail.com>
2019-12-04 20:27:28 -0700

    Stuuuufff

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,8 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoUsers from './mongoUsers';
 import mongoPosts from './mongoPosts';
-import posts from './posts';
-import items from './items';
+import mongoItems from './mongoItems';
 import cors from 'cors';
 
 const app = express();
@@ -27,7 +26,10 @@ app.post('/api/users/login/', async (req, res) => {
 
 // app.get('/api/users/:id', [users.getUser]);
 
-app.get('/api/posts/:userID', [posts.getUserPosts])
+app.get('/api/posts/:userID', async (req, res) => {
+    let posts = await mongoPosts.getUserPosts(req.params.userID);
+    res.status(200).send(posts.slice(0, 20));
+});
 
 app.get('/api/posts', async (req, res) => {
     let keyword = req.query.keyWords || '';
@@ -40,11 +42,17 @@ app.post('/api/posts', async (req, res) => {
     res.send(200);
 });
 
-app.delete('/api/posts/:postID', [posts.deletePost])
+app.delete('/api/posts/:postID', async (req, res) => {
+    await mongoPosts.deletePost(req.params.postID);
+    res.sendStatus(204);
+});
 
 // app.put('/api/posts/:postID', [posts.updatePost])
 
-app.get('/api/items', [items.getItems]);
+app.get('/api/items', async (req, res) => {
+    let items = await mongoItems.getItems();
+    res.status(200).send(items.splice(0, 100));
+});
 
 app.listen(8080, () =>
     console.log(`Application listening on port 8080`),
